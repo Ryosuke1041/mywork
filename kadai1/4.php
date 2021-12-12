002675fe7c58b4e9a138065dd7ca58c4b44fff23
Ryosuke1041 <rekyuso11@gmail.com>
2021-12-12 15:44:24 +0900

    4

--- kadai1/4.php
+++ kadai1/4.php
@@ -1,6 +1,11 @@
 <?php
+if(isset($_GET['name'])){
+    echo $_GET['name'];
+}
 
 ?>
-<form action="action.php" method="post">
-<input type="submit" />
+<form action="4.php" method="get">
+名前: <input type="text" name="name" />
+年齢: <input type="text" name="age" />
+<input type="submit" value ="おくるよーん">
 </form>
