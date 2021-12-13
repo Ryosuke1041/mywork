9d492676cdaacbeb1d13067fad09ce55b16a0fbe
Ryosuke1041 <rekyuso11@gmail.com>
2021-12-13 19:02:07 +0900

    5.php

--- kadai1/5.php
+++ kadai1/5.php
@@ -1,3 +1,11 @@
 <?php
-fopen("4.php");
+if(isset($_GET['name'])){
+    echo $_GET['name'];
+    file_put_contents("5.txt",$_GET['name']);
+}
 ?>
+<form action="5.php" method="get">
+名前: <input type="text" name="name" />
+年齢: <input type="text" name="age" />
+<input type="submit" value ="おくるよーん">
+</form>
