4db8db32d005f5d1d02bd002424a05033b62c6ba
Ryosuke1041 <rekyuso11@gmail.com>
2021-12-18 15:13:46 +0900

    62

--- kadai1/6.php
+++ kadai1/6.php
@@ -1,6 +1,14 @@
 <?php
-$Z = fopen("5.php","a");
-fwrite($Z,"");
-fclose($Z);
+if(isset($_GET['name'])){
+    echo $_GET['name'];
+    $Z = fopen("6.txt","a");
+    fwrite($Z,$_GET['name']."\n");
+    fclose($Z);
+}
 
 ?>
+<form action="6.php" method="get">
+名前: <input type="text" name="name" />
+年齢: <input type="text" name="age" />
+<input type="submit" value ="おくるよーん">
+</form>
