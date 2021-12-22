621520850970e70c9dde50b84fd6bd6d80eeb798
Ryosuke1041 <rekyuso11@gmail.com>
2021-12-23 00:15:40 +0900

    98

--- kadai2/2.php
+++ kadai2/2.php
@@ -1,23 +1,23 @@
 <?php
-if(isset($_GET['name'])){
-    $name = $_GET['name'];
-}
-if(isset($_GET['comment'])){
-    $comment = $_GET['comment'];
-}
+date_default_timezone_set('Japan');
+
 if(file_exists("9.txt")){
     $number = count(file("9.txt")) + 1;
 }else{
     $number = 1;
 }
-$date = date("Y/n/j G:I:s");
-$Z = fopen("9.txt","a");
+$date = date("Y/m/d H:i:s");
+if(isset($_GET['name']) && isset($_GET['comment'])){
+    $name = $_GET['name'];
+    $comment = $_GET['comment'];
+    $Z = fopen("9.txt","a");
     fwrite($Z, $number."<>".$name."<>".$comment."<>".$date."\n");
     fclose($Z);
+}
 
 ?>
 
-<form action="9.php" method="get">
+<form action="2.php" method="get">
 名前: <input type="text" name="name" />
 コメント: <input type="text" name="comment" />
 <input type="submit" value ="送信">
