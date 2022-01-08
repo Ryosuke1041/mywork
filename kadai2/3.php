b8fda2ea1917eb00edb86d4b0f96993d5b7bbfbe
Ryosuke1041 <rekyuso11@gmail.com>
2022-01-08 14:10:42 +0900

    3

---
+++ kadai2/3.php
@@ -0,0 +1,32 @@
+<?php
+date_default_timezone_set('Japan');
+
+if(file_exists("9.txt")){
+    $number = count(file("9.txt")) + 1;
+}else{
+    $number = 1;
+}
+$date = date("Y/m/d H:i:s");
+if(isset($_GET['name']) && isset($_GET['comment'])){
+    $name = $_GET['name'];
+    $comment = $_GET['comment'];
+    $Z = fopen("9.txt","a");
+    fwrite($Z, $number."<>".$name."<>".$comment."<>".$date."\n");
+    fclose($Z);
+}
+
+?>
+
+<form action="3.php" method="get">
+名前: <input type="text" name="name" />
+コメント: <input type="text" name="comment" />
+<input type="submit" value ="送信">
+</form>
+<?php
+$Y = file("9.txt");
+$X = explode("<>", $Y);
+foreach($X as $value){
+    echo $value."<br>";
+   }
+
+?>
